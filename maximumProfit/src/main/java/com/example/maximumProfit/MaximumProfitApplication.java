cf128a1ffe185ce3fc1875f06341f9c8e9c80014
Chanon Limvongrujirat <ohm.191@hotmail.com>
2025-02-03 20:25:52 +0700

    init

---
+++ maximumProfit/src/main/java/com/example/maximumProfit/MaximumProfitApplication.java
@@ -0,0 +1,13 @@
+package com.example.maximumProfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximumProfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaximumProfitApplication.class, args);
+	}
+
+}
---
+++ maximumProfit/src/test/java/com/example/maximumProfit/MaximumProfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maximumProfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaximumProfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
